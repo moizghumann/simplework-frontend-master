3efc46a8f67c2ea9fbfdef0564c72a1a7cb7bcc7
Moiz Ghuman <moiz.shaukat@aerodyne.group>
2025-02-23 23:56:40 +0500

    add utility functions, update configuration, and implement AdminDashboardChart component

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,18 +1,22 @@
 import SideMenu from "../../components/SideMenu/SideMenu";
-import DashboadAllSalesChart from "../../components/Charts/DashboadAllSalesChart";
 import DashboardCards from "../../components/Cards/DashboardCards";
+import AdminDashboardChart from "@/components/Charts/AdminDashboardChart";
 
 const AdminDashboard = () => {
     return (
         <div className="px-5 flex w-full">
           <SideMenu />
-          <div className="w-full flex flex-col items-start text-white fade-in">
-            <div className="flex pt-4 px-4">
-              <DashboadAllSalesChart />
-              <DashboardCards align={"l"} type={1} />
+          {/* <div className="w-full flex flex-col items-start text-white fade-in"> */}
+          <div className="grid grid-cols-6 grid-rows-6 gap-0 text-white fade-in">
+            <div className="col-span-4 row-span-4 pt-4 px-4"
+            data-aos="fade-right">
+              <AdminDashboardChart/>
             </div>
-            <div className="px-2 py-8">
+            <div className="col-span-2 row-span-4 col-start-5 mt-10">
               <DashboardCards align={"r"} type={2} />
+            </div>
+            <div className="col-span-6 row-span-2 row-start-5 flex px-2 py-8">
+              <DashboardCards align={"l"} type={1} />
             </div>
           </div>
         </div>
